fcece14e00ff0e1c5c269baf42ad758fbbee2119
Thomari01 <tsutters@students.mchenry.edu>
2025-05-31 19:19:43 -0500

    Added Python program for conversion
    Python Knock Knock joke

---
+++ main.py
@@ -0,0 +1,7 @@
+﻿print("Knock, knock.\nWho's there?\nNacho.\nNacho who?\nNacho cheese!")
+print("Knock, knock.")  
+print("Who's there?")  
+print("Nacho.")  
+print("Nacho who?")  
+print("Nacho cheese!") 
+# Knock-Knock Joke Program
