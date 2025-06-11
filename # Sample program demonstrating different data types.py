dadff31b720c5857bfc14616be8a03f1e87f7660
Thomari01 <tsutters@students.mchenry.edu>
2025-06-11 00:34:04 -0500

    Add files via upload
    Sample program demonstrating different data types

---
+++ # Sample program demonstrating different data types.py
@@ -0,0 +1,17 @@
+print("Literal:", 25)
+
+# Integer
+y = -50
+print("Integer:", y)
+
+# Float
+z = 15.5
+print("Float:", z)
+
+# String
+name = "Tom"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
