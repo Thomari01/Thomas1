e1fd9772afbdf525b531a70c6ac93bc10507a194
Thomari01 <tsutters@students.mchenry.edu>
2025-06-20 02:30:18 -0500

    Add files via upload
    Decision in the PYTHON assignment resubmission

---
+++ Decision.py
@@ -0,0 +1,17 @@
+age = int(input("Please enter your age: "))
+if age >= 12:
+    print(f"At {age} years old, you are old enough to drive.")
+else:
+    print(f"At {age} years old, you are not old enough to drive yet.")
+if age >= 19:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
+if age >= 27:
+    print("You can legally buy alcohol.")
+else:
+    print("You are not legally allowed to buy alcohol yet.")
+if age >= 60:
+    print("You are eligible for a senior discount.")
+else:
+    print("You are not eligible for a senior discount yet.")
