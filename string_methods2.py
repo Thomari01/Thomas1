44bea9b994ba7d157a906730fe92f1c05b033b2b
Thomari01 <tsutters@students.mchenry.edu>
2025-07-13 18:31:58 -0500

    Add files via upload
    PE2 Module 2.3: String Methods

---
+++ string_methods2.py
@@ -0,0 +1,43 @@
+string1 = "python"
+print(string1.capitalize())  
+string2 = "python"
+print(string2.center(10))  
+string3 = "python"
+print(string3.endswith("on"))  
+string4 = "python"
+print(string4.find("th"))  
+string5 = "python3"
+print(string5.isalnum())  
+string6 = "python"
+print(string6.isalpha())  
+string7 = "python"
+print(string7.islower())  
+string8 = " "
+print(string8.isspace())  
+string9 = "PYTHON"
+print(string9.isupper())  
+iterable1 = ["Python", "is", "fun"]
+separator = "-"
+print(separator.join(iterable1))  
+string10 = "PYTHON"
+print(string10.lower())  
+string11 = " python"
+print(string11.lstrip())  
+string12 = "python "
+print(string12.rstrip())  
+string13 = "I love python"
+print(string13.replace("python", "snake"))  
+string14 = "python"
+print(string14.rfind("n"))  
+string15 = "python-is-fun"
+print(string15.split("-"))  
+string16 = "python"
+print(string16.startswith("py"))  
+string17 = " python "
+print(string17.strip())  
+string18 = "Python"
+print(string18.swapcase())  
+string19 = "python is fun"
+print(string19.title())  
+string20 = "python"
+print(string20.upper())  
