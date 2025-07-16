0882c9368be1aab7ed794b4803ff3d3ee1568037
Thomari01 <tsutters@students.mchenry.edu>
2025-07-16 11:38:54 -0500

    Add files via upload
    Re-submission

--- ascii_converter.py
+++ ascii_converter.py
@@ -1,10 +1,11 @@
 def main():
     user_input = input("Enter a single character: ")
     if len(user_input) != 1:
-     print("Error: You must enter exactly one character.")
+        print("Error: You must enter exactly one character.")
     else:
-     ascii_value = ord(user_input)
-    print(f"The ASCII value of '{user_input}' is {ascii_value}.")
+        ascii_value = ord(user_input)
+        print(f"The ASCII value of '{user_input}' is {ascii_value}.")
+
     try:
         ascii_input = int(input("Enter an ASCII value (32 to 127): "))
         if 32 <= ascii_input <= 127:
@@ -14,5 +15,6 @@ def main():
             print("Error: Value must be between 32 and 127.")
     except ValueError:
         print("Error: You must enter a valid integer.")
+
 if __name__ == "__main__":
     main()
