d16154c1b8d4002e01222ac85778b6b597291752
Thomari01 <tsutters@students.mchenry.edu>
2025-07-20 21:19:09 -0500

    Add files via upload
    PE2 Module 3.5.1.1 OOP Fundamentals: Inheritance

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-from math_operations import calculator
+# main.py
+from employee import ProductionWorker
+def main():
+    print("Enter the following details of the Employee")
+    print("--------------------------------------------")
+    
+    name = input("Enter Employee Name: ")
+    number = input("Enter Employee Number: ")
+    pay_rate = float(input("Enter Pay Rate: "))
+    shift_number = int(input("Enter Shift Number (1 for Day, 2 for Night): "))
+    
+    worker = ProductionWorker(name, number, shift_number, pay_rate)
 
-result = calculator.add(7, 2)
-print("Result:", result)
+    print("-------------------------------------------------------")
+    print("Details of Employee:")
+    print("-------------------------------------------------------")
+    print("Name:", worker.get_name())
+    print("Employee Number:", worker.get_number())
+    
+    if worker.get_shift_number() == 1:
+        shift_text = "Day"
+    elif worker.get_shift_number() == 2:
+        shift_text = "Night"
+    else:
+        shift_text = "Unknown"
+
+    print("Shift:", shift_text)
+    print("Pay Rate:", worker.get_pay_rate())
+
+if __name__ == "__main__":
+    main()
